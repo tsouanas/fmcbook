1fc607f560087a1924578b63c8e04ec0cf4b12ef
Thanos Tsouanas <thanos@tsouanas.org>
2017-05-29 02:03:01 -0300

    Dirty tools not needed anymore

--- tools/checkhints.py
+++
@@ -1,8 +0,0 @@
-import sys
-tex = file('fmcmain.tex','r')
-prev = ''
-for (i,line) in enumerate(tex):
-    if (prev.strip() != "") and (line.startswith('\\hin') or line.startswith('\\sol')):
-        print "\hint or \solution without ending paragraph.  Line:", i
-        sys.exit(1)
-    prev = line
--- tools/parizeex.py
+++
@@ -1,7 +0,0 @@
-tex = file('fmcmain.tex','r')
-prev = ''
-for (i,line) in enumerate(tex):
-    if prev.strip() and (line.strip() == '\endexercise'):
-        print ""
-    print line,
-    prev = line
