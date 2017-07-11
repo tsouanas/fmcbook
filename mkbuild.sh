52e9030d9592b9ef51e608004734de69e852a74e
Thanos Tsouanas <thanos@tsouanas.org>
2017-07-11 15:11:45 +0300

    better mk scripts

--- mkbuild.sh
+++ mkbuild.sh
@@ -2,6 +2,10 @@
 
 # first compile
 xetex fmc
+if [ $? -ne 0 ]; then
+    echo "Error during first compilation."
+    exit 1
+fi
 
 # get lang and date
 FMCLANG=`cat fmc.lang`
@@ -12,15 +16,20 @@ bibtex fmc \
   && xetex fmc \
   && makeindex -o fmc.ind fmc.idx \
   && makeindex -o fmc.nnd fmc.ndx \
-  && xetex fmc
+  && xetex fmc \
+  && cp fmc.pdf fmc-${FMCLANG}.pdf
+if [ $? -ne 0 ]; then
+    echo "Error during full compilation."
+    exit 1
+fi
 
 # find pagecount
 grep 'Output written on fmc.pdf' fmc.log | egrep -o '[0-9]+' > fmc-${FMCLANG}.pages
 
+# create .last and .lasttag
 echo -n $FMCLAST > fmc-${FMCLANG}.last
 echo -n $FMCLAST | sed 's/[^0-9]//g' > fmc-${FMCLANG}.lasttag
 
+# create .webtoc
 grep '\\tocchapterentry\ ' fmc.toc | sed 's/^.tocchapterentry {/<li>/' | sed 's/}{[0-9]*}{[0-9]*}$//' > fmc-${FMCLANG}.webtoc
-
-cp fmc.pdf fmc-${FMCLANG}.pdf
 
--- mkupload.sh
+++ mkupload.sh
@@ -5,11 +5,15 @@ FMCLASTTAG=`cat fmc-${FMCLANG}.lasttag`
 HOSTNAME=tsouanas.org
 HOSTPATH=fmcbook/
 
+# tag pdf
 cp fmc-${FMCLANG}.pdf fmc-${FMCLANG}-${FMCLASTTAG}.pdf
 
+# upload pdf and metafiles
 scp fmc-${FMCLANG}-${FMCLASTTAG}.pdf fmc-${FMCLANG}.{last,lasttag,pages,webtoc} $HOSTNAME:$HOSTPATH
 
+# execute lastonly.sh remotely to get rid of old pdf files
 ssh $HOSTNAME "cd fmcbook && sh lastonly.sh ${FMCLANG}"
 
+# remove tagged pdf
 rm -f fmc-${FMCLANG}-${FMCLASTTAG}.pdf
 
