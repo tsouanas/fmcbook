760c6b2bc5bee5729f541e626a96cd4947274e4a
Thanos Tsouanas <thanos@tsouanas.org>
2017-08-28 17:17:03 -0300

    add currying.py code

---
+++ code/currying.py
@@ -0,0 +1,16 @@
+# assume that f is given
+def f(x,y):
+    pass
+
+# without lambdas
+def F(x):
+    def tmp(y):
+        return f(x,y)
+    return tmp
+
+# with lambdas
+def F(x):
+    return lambda y: f(x,y)
+
+# To compute f(x,y) using F, call F(x)(y)
+# ...which is (F(x))(y) really.
