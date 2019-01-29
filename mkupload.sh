968b49979eb8f021f00dcac5373433f34680cf6c
Thanos Tsouanas <thanos@tsouanas.org>
2019-01-28 22:10:07 -0300

    completely staticize site

--- mkupload.sh
+++ mkupload.sh
@@ -8,11 +8,11 @@ HOSTPATH=fmcbook/
 # tag pdf
 cp fmc-${FMCLANG}.pdf fmc-${FMCLANG}-${FMCLASTTAG}.pdf
 
-# upload pdf and metafiles
-scp fmc-${FMCLANG}-${FMCLASTTAG}.pdf fmc-${FMCLANG}.{last,lasttag,pages,webtoc} $HOSTNAME:$HOSTPATH
+# upload .pdf, .bib, and metafiles
+rsync -av fmc-${FMCLANG}-${FMCLASTTAG}.pdf fmcbook.bib fmc-${FMCLANG}.{last,lasttag,pages,webtoc} $HOSTNAME:$HOSTPATH
 
-# execute lastonly.sh remotely to get rid of old pdf files
-ssh $HOSTNAME "cd fmcbook && sh lastonly.sh ${FMCLANG}"
+# execute scripts remotely to get rid of old pdf files and make the site
+ssh $HOSTNAME "cd fmcbook && sh lastonly.sh ${FMCLANG} && sh mksite.sh ${FMCLANG}"
 
 # remove tagged pdf
 rm -f fmc-${FMCLANG}-${FMCLASTTAG}.pdf
